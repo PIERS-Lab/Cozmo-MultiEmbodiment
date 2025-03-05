674348afa141c072a4194c3230917852f84df54b
Alex Nunes <Alexxnunes2004@gmail.com>
2025-03-05 14:27:27 -0500

    Implemented cube relocation, and have a multi driver test set up for it.

--- cozCube.py
+++ cozCube.py
@@ -23,8 +23,8 @@ class coz:
         await self.system.say_text("I can't do that! " + detail).wait_for_completed()
 
     #It needs to take input as apart of analyzing the task
+    #returns the found lightcube object
     async def findCube(self, cbID):
-        import ipdb; 
         await self.system.set_head_angle(degrees(0)).wait_for_completed()
         if (cbID != self.cubeID):
             await self.failmsg(detail = "as I don't own this cube")
@@ -36,27 +36,43 @@ class coz:
         currBehavior = self.system.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
         try: 
             found = await self.system.world.wait_for_observed_light_cube(timeout = 20)
-            print(found.cube_id)
+            #print(found.cube_id)
             while (int(found.cube_id) != int(cbID)):
                 #print("Found has ID: ", found.cube_id, "\n", "cbID: ", cbID)
                 found = await self.system.world.wait_for_observed_light_cube(timeout = 10, include_existing=False)
                 #if we can't find the right cube, fail
-            print("exited loop!\n")
+            #print("exited loop!\n")
         except asyncio.TimeoutError:
             cozmo.behavior.Behavior.stop(currBehavior)
             await self.system.say_text("I couldn't find my cube", use_cozmo_voice=True).wait_for_completed()
             
-            print("returning!\n")
+            #print("returning!\n")
             return False
         #print("starting Cube Recognition process\n")    
         cozmo.behavior.Behavior.stop(currBehavior)
-        await self.system.say_text("Cube Found!", play_excited_animation=True,use_cozmo_voice=True).wait_for_completed()
-        #Cozmo has some sort of way to align with the cube, figure this out and use it
-        #try just picking up the cube and moving it to it's start for now, command processing is still a ways away.....
+        #await self.system.say_text("Cube Found!", play_excited_animation=True,use_cozmo_voice=True).wait_for_completed()
         
-        return True
-
-    #Reconizing a certain position/destination
+        
+        return found
+    #end point will be a cozmo pose
+    # if return _to_start is set to true
+    # cozmo will Ignore the end point argument and just return to his starting position if not given one
+    async def moveCube(self, cbID, endpoint = cozmo.util.Pose(0,0, 0, angle_z=cozmo.util.degrees(0)), return_to_start = False):
+        temp = await self.findCube(cbID)
+        if (temp == False):
+            return False
+        #cozmo.robot.Robot.dock_with_cube(self,temp, None, cozmo.robot_alignment.RobotAlignmentTypes.LiftPlate, None, False, 3)
+        #cozmo.robot.Robot.set_lift_height(self, 1.0)
+        #cozmo.robot.Robot.go_to_pose(endpoint)
+        # cozmo.robot.Robot.set_lift_height( 0.0)
+        # cozmo.robot.Robot.drive_straight(-5, 1)
+        await self.system.dock_with_cube(temp, None, cozmo.robot_alignment.RobotAlignmentTypes.LiftPlate, None, False, 3).wait_for_completed()
+        await self.system.set_lift_height(1.0).wait_for_completed()
+        await self.system.go_to_pose(endpoint).wait_for_completed()
+        await self.system.set_lift_height(0).wait_for_completed()
+        #back away from cube to avoid messing with the cube
+        await self.system.drive_straight(cozmo.util.distance_inches(-1), cozmo.util.speed_mmps(100)).wait_for_completed()
+    #Recongizing a certain position/destination
     #   color detection?
     #   Work on recognition of unique objects
     #
--- coz_driver.py
+++ coz_driver.py
@@ -1,13 +1,27 @@
+#this driver will test the custom functionality made  for the trust and teamwork project
 from cozCube import coz
 import cozmo
-from cozmo import * 
+from cozmo import *
+
 import asyncio
-#note for later: implement differant control paths to determine how many connections are to be made
-async def test(connection):
+async def test_find(connection):
+    print("testing cube recognition")
     print ("Please input cube ID to assign the cozmo to: ")
     testiee = coz(await connection.wait_for_robot(), input())
     print ("please input the cube Id that cozmo should grab: ")
-    await testiee.findCube(input())
-    print("i'm out!\n")
-    return None
-cozmo.connect_with_tkviewer(test)
+    result =await testiee.findCube(input())
+    if (result):
+        testiee.system.say_text("Cube Found!", play_excited_animation=True,use_cozmo_voice=True).wait_for_completed()
+    #print("i'm out!\n")
+    return 
+
+async def test_move(connection):
+    print("Testing cube relocation")
+    print ("Please input cube ID to assign the cozmo to: ")
+    ID = input()
+    testiee = coz(await connection.wait_for_robot(), ID)
+    await testiee.moveCube(ID)
+
+
+
+cozmo.connect_with_tkviewer(test_move)
