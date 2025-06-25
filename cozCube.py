917b5b18931fb0f84192fd725ae7821ef8db8f15
Alex Nunes <Alexxnunes2004@gmail.com>
2025-06-25 12:49:52 -0400

    Refactored to add individual lift and drop cube commands

--- cozCube.py
+++ cozCube.py
@@ -6,6 +6,7 @@ import cozmo
 from cozmo import *
 from cozmo.util import degrees
 
+
 #this is like the active connection, so most actions the cozmo will take are used through this object
 #cozmo.conn.robot.Robot.
 #cozmo.robot.Robot.
@@ -13,11 +14,13 @@ from cozmo.util import degrees
 class coz:
 
     def __init__ (self, system, cube_Num):
-        #system will  be a cozmo.conn.cozmoConnection.robot.Robot object
+        #system will be a cozmo.conn.cozmoConnection.robot.Robot object
         self.system = system
         self.cubeID = cube_Num
         print("constructor called!\n")
-
+        # set up goal markers Goals are x by x by x at their base, a wall is used due to other options being not suitable
+        
+        
     async def failmsg(self, detail = "."):
         #self.system.robot.Robot.say_text("My apolgies, But I am unable to fufill your request " + detail, use_cozmo_voice=True)
         await self.system.say_text("I can't do that! " + detail).wait_for_completed()
@@ -30,9 +33,8 @@ class coz:
             await self.failmsg(detail = "as I don't own this cube")
             return
         #look for cube
-        
-        #this line is causing some bad mojo jojo 
-       
+        #To-Do: make this more robust, 
+        #have cozmo search a little harder (maybe have him move around to account for the poor range of his vision)
         currBehavior = self.system.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
         try: 
             found = await self.system.world.wait_for_observed_light_cube(timeout = 20)
@@ -54,7 +56,17 @@ class coz:
         
         
         return found
-    #end point will be a cozmo pose
+    async def lift_cube(self, target):
+        await self.system.dock_with_cube(target, num_retries=3, approach_angle=cozmo.util.degrees(0)).wait_for_completed()
+        await self.system.set_lift_height(1.0).wait_for_completed()
+
+    async def drop_cube(self):
+        await self.system.set_lift_height(0)
+        #back away from cube to avoid messing with it accidentally
+        await self.system.drive_straight(cozmo.util.distance_inches(-1), cozmo.util.speed_mmps(100)).wait_for_completed()
+
+        
+    # end point is a cozmo pose
     # if return _to_start is set to true
     # cozmo will Ignore the end point argument and just return to his starting position if not given one
     async def moveCube(self, cbID, endpoint = cozmo.util.Pose(0,0, 0, angle_z=cozmo.util.degrees(0))):
@@ -66,13 +78,9 @@ class coz:
         #cozmo.robot.Robot.go_to_pose(endpoint)
         # cozmo.robot.Robot.set_lift_height( 0.0)
         # cozmo.robot.Robot.drive_straight(-5, 1)
-        await self.system.dock_with_cube(temp, None, cozmo.robot_alignment.RobotAlignmentTypes.LiftPlate, None, False, 3).wait_for_completed()
-        await self.system.set_lift_height(1.0).wait_for_completed()
+        await self.lift_cube(temp)
         await self.system.go_to_pose(endpoint).wait_for_completed()
-        await self.system.set_lift_height(0).wait_for_completed()
-        #back away from cube to avoid messing with the cube
-        await self.system.drive_straight(cozmo.util.distance_inches(-1), cozmo.util.speed_mmps(100)).wait_for_completed()
-    #Recongizing a certain position/destination
-    #   color detection?
-    #   Work on recognition of unique objects
+        await self.drop_cube()
+    # use custom objects to reconize custom goal, will likely have to do something wierd to work with restrictions
+    #   
     #
