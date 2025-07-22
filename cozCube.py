1ee8eddcd7b18d6ef7e5d219e7513d532b0bf95c
Alex Nunes <Alexxnunes2004@gmail.com>
2025-07-22 14:09:48 -0400

    added a run game file and implemented base behavior, as well as changing the deliver function to work with cozmo's custom goals

--- cozCube.py
+++ cozCube.py
@@ -6,61 +6,69 @@ import cozmo
 from cozmo import *
 from cozmo.util import degrees
 from cozmo.objects import CustomObjectMarkers
+import math
 class coz:
 
-    def __init__ (self, system, cube_Num):
+    def __init__ (self, robot, cube_Num):
         # robot is a cozmo.conn.cozmoConnection.robot.Robot object
-        self.robot = system
-        self.cubeID = cube_Num
+        self._robot = robot
+        self._cubeID = cube_Num
         # from the person's position, goal 0 is far left, goal 1 is middle, goal 2 is far right
-        self.goals = None
-    async def create(system, cube_num):
-        self = coz(system, cube_num)
-        self.goals = [await self.robot.world.define_custom_wall(cozmo.objects.CustomObjectTypes.CustomType02,
+        self._goals = None
+        self._alignDistmm = 200
+    async def create(robot, cube_num):
+        self = coz(robot, cube_num)
+        self._goals = [await self._robot.world.define_custom_wall(cozmo.objects.CustomObjectTypes.CustomType01,
                                               CustomObjectMarkers.Triangles5,
-                                              150, 120,
-                                              132, 132, True)]
+                                              100, 120,
+                                              40, 40, True), 
+                                              await self._robot.world.define_custom_wall(cozmo.objects.CustomObjectTypes.CustomType02,
+                                              CustomObjectMarkers.Circles5,
+                                              100, 120,
+                                              40, 40, True), await self._robot.world.define_custom_wall(cozmo.objects.CustomObjectTypes.CustomType03,
+                                              CustomObjectMarkers.Hexagons5,
+                                              100, 120,
+                                              40, 40, True)]
         # set up goal markers Goals are x by x by x (still wip) at their base, a wall is used due to other options being not suitable
         return self
-        
     async def failmsg(self, detail = "."):
-        await self.robot.say_text("I can't do that! " + detail).wait_for_completed()
+        await self._robot.say_text("I can't do that! " + detail).wait_for_completed()
 
     # It needs to take input as apart of analyzing the task
     # returns the found lightcube object
     async def findCube(self, cbID):
-        await self.robot.set_head_angle(degrees(0)).wait_for_completed()
-        if (cbID != self.cubeID):
+        await self._robot.set_head_angle(degrees(0)).wait_for_completed()
+        if (cbID != self._cubeID):
             await self.failmsg(detail = "as I don't own this cube")
             return
         # look for cube
         ''' To-Do: make this more robust, 
         have cozmo search a little harder (maybe have him move around to account for the poor range of his vision)'''
-        currBehavior = self.robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+        currBehavior = self._robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
         try: 
-            found = await self.robot.world.wait_for_observed_light_cube(timeout = 20)
+            found = await self._robot.world.wait_for_observed_light_cube(timeout = 40)
             while (int(found.cube_id) != int(cbID)):
-                found = await self.robot.world.wait_for_observed_light_cube(timeout = 10, include_existing=False)
+                found = await self._robot.world.wait_for_observed_light_cube(timeout = 40, include_existing=False)
                 # if we can't find the right cube, fail
         except asyncio.TimeoutError:
             cozmo.behavior.Behavior.stop(currBehavior)
-            await self.robot.say_text("I couldn't find my cube", use_cozmo_voice=True).wait_for_completed()
+            await self._robot.say_text("I couldn't find my cube", use_cozmo_voice=True).wait_for_completed()
             return False
         cozmo.behavior.Behavior.stop(currBehavior)
         
         
         return found
     async def lift_cube(self, target):
-        await self.robot.dock_with_cube(target, num_retries=3, approach_angle=cozmo.util.degrees(0)).wait_for_completed()
-        await self.robot.set_lift_height(1.0).wait_for_completed()
+        await self._robot.dock_with_cube(target, num_retries=3, approach_angle=cozmo.util.degrees(0)).wait_for_completed()
+        await self._robot.set_lift_height(1.0).wait_for_completed()
 
     async def drop_cube(self):
-        await self.robot.set_lift_height(0).wait_for_completed()
+        await self._robot.set_lift_height(0).wait_for_completed()
         # back away from cube to avoid messing with it accidentally
-        await self.robot.drive_straight(cozmo.util.distance_inches(-1), cozmo.util.speed_mmps(100)).wait_for_completed()
+        await self._robot.drive_straight(cozmo.util.distance_mm(-100), cozmo.util.speed_mmps(100)).wait_for_completed()
     # if cozmo fails for any reason, false is returned, other wise the pose of the desired object is returned instead
     async def find_goal(self, goalNum):
-        await self.robot.set_head_angle(degrees(0)).wait_for_completed()
+        await self._robot.set_head_angle(degrees(0)).wait_for_completed()
         if goalNum < 0 or goalNum > 2:
             self.failmsg("as that goal is not real!")
             return False 
@@ -70,14 +78,15 @@ class coz:
         # have cozmo search a little harder (maybe have him move around to account for the poor range of his vision)
         try:
             # check if we found the correct goals
-            currBehavior = self.robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-            found = await self.robot.wait_for(cozmo.objects.EvtObjectObserved,  timeout = 40)
-            while (isinstance(found.obj, cozmo.objects.LightCube) or self.goals[goalNum].object_type != found.obj.object_type):
-                found = await self.robot.wait_for(cozmo.objects.EvtObjectObserved,  timeout = 40)
+            currBehavior = self._robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+            found = await self._robot.wait_for(cozmo.objects.EvtObjectObserved,  timeout = 40)
+            while (isinstance(found.obj, cozmo.objects.LightCube) or isinstance(found.obj, cozmo.objects.Charger) or 
+                   self._goals[goalNum].object_type != found.obj.object_type):
+                found = await self._robot.wait_for(cozmo.objects.EvtObjectObserved,  timeout = 40)
                 
         except asyncio.TimeoutError:
             cozmo.behavior.Behavior.stop(currBehavior)
-            await self.robot.say_text("I couldn't find the goal", use_cozmo_voice=True).wait_for_completed()
+            await self._robot.say_text("I couldn't find the goal", use_cozmo_voice=True).wait_for_completed()
             return False 
         print("goal found!") 
         cozmo.behavior.Behavior.stop(currBehavior)
@@ -89,13 +98,21 @@ class coz:
         the goals as a wall, so an offset must be applied so cozmo arrives at the correct location
         Also: this current offset will not be finalized until a goal design is complete '''
         # this must be done like this, the API does not like existing poses being edited for some reason
-        dest = cozmo.util.Pose(goal.pose._position.x - 144, goal.pose._position.y, goal.pose.position.z, 
+
+        #calculate the x and y offsets using pythagoreans theorem
+        xAlignOff = self._alignDistmm * math.cos(goal.pose._rotation.angle_z.radians)
+        yAlignOff = self._alignDistmm * math.sin(goal.pose._rotation.angle_z.radians)
+        destSetup = cozmo.util.Pose(goal.pose.position.x - xAlignOff, goal.pose.position.y - yAlignOff, goal.pose.position.z, 
                                angle_z=goal.pose._rotation.angle_z, origin_id=goal.pose._origin_id )
-        await self.robot.go_to_pose(dest).wait_for_completed()
+        # dest = cozmo.util.Pose(xdelivOff, ydelivOff, goal.pose.position.z, 
+        #                        angle_z=goal.pose._rotation.angle_z, origin_id=goal.pose._origin_id )
+        # align Cozmo with the goal entrance
+        await self._robot.go_to_pose(destSetup).wait_for_completed()
+        #await self._robot.go_to_pose(dest).wait_for_completed()
+        #enter goal and deliver
+        await self._robot.drive_straight(cozmo.util.distance_mm(100), cozmo.util.speed_mmps(100)).wait_for_completed()
         await self.drop_cube()
 
-
-        
     # end point is a cozmo pose
     '''  if return _to_start is set to true cozmo will Ignore the end point argument 
     and just return to his starting position if not given one '''
@@ -104,5 +121,14 @@ class coz:
         if (temp == False):
             return False
         await self.lift_cube(temp)
-        await self.robot.go_to_pose(endpoint).wait_for_completed()
+        await self._robot.go_to_pose(endpoint).wait_for_completed()
         await self.drop_cube()
+
+    async def set_cube_ownership(self, cubeID):
+        self._cubeID = cubeID
+
+
+    async def reset_position(self):
+         await self._robot.set_head_angle(degrees(0)).wait_for_completed()
+         await self._robot.set_lift_height(0).wait_for_completed() 
+         await self._robot.go_to_pose(cozmo.util.Pose(0,0, 0, angle_z=cozmo.util.degrees(0))).wait_for_completed()
--- coz_driver.py
+++ coz_driver.py
@@ -1,4 +1,4 @@
-#this driver will test the custom functionality made for the norm violation project
+# this driver will test the custom functionality made for the norm violation project
 # edit the function being called at the bottom to use differant tests!
 from cozCube import coz
 import cozmo
@@ -12,7 +12,7 @@ async def test_find_cube(connection):
     print ("please input the cube Id that cozmo should grab: ")
     result = await testiee.findCube(input())
     if (result):
-        testiee.robot.say_text("Cube Found!", play_excited_animation=True,use_cozmo_voice=True).wait_for_completed()
+        testiee._robot.say_text("Cube Found!", play_excited_animation=True,use_cozmo_voice=True).wait_for_completed()
     #print("i'm out!\n")
     return 
 
@@ -28,7 +28,7 @@ async def test_find_goal(connection):
     testiee = await coz.create(await connection.wait_for_robot(), 1)
     result = await testiee.find_goal(0)
     if (result != False):
-        await testiee.robot.say_text("Goal Found!", play_excited_animation=True, use_cozmo_voice=True).wait_for_completed()
+        await testiee._robot.say_text("Goal Found!", play_excited_animation=True, use_cozmo_voice=True).wait_for_completed()
         return
     return
 
@@ -36,6 +36,7 @@ async def test_find_goal(connection):
 async def test_deliver(connection):
     print("Testing Goal Identification with goal 1")
     testiee = await coz.create(await connection.wait_for_robot(), 1)
+    print(testiee._goals)
     print("finding cube")
     cube = await testiee.findCube(1)
     if(cube == False):
@@ -53,4 +54,25 @@ async def test_deliver(connection):
     await testiee.deliver(goalPose)
     return
 
+async def test_return(connection):
+    print("Testing Goal Identification with goal 1")
+    testiee = await coz.create(await connection.wait_for_robot(), 1)
+    print(testiee._goals)
+    print("finding cube")
+    cube = await testiee.findCube(1)
+    if(cube == False):
+        print("couldn't find cube!")
+        return
+    print ("finding goal")
+    goalPose = await testiee.find_goal(0)
+    if(goalPose == False):
+        print ("could not find goal")
+        return
+    print("grabbing cube")
+    await testiee.lift_cube(cube)
+    print("delivering cube to goal")
+    print(goalPose)
+    await testiee.deliver(goalPose)
+    await testiee.reset_position()
+    return
 cozmo.connect_with_3dviewer(test_deliver, enable_camera_view=True)
